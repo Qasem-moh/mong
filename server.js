1fdcba6040a041a2a9a78a723d40f490a2c6d325
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-14 19:28:18 +0300

    mongo

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port=5410
 
 app.use(express.json())
 const routes = require('./routes/routes');
-app.use('/api', routes)
+app.use('/', routes)
 // const url="mongodb://localhost:27017/person"
 // const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/?retryWrites=true&w=majority"
 const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/testDatabase"
