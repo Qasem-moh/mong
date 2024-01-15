fa4c79d29fe255c6970e48d5ed3dc3dced7a7839
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-15 14:14:56 +0300

    mongo

--- server.js
+++ server.js
@@ -2,15 +2,13 @@ const express=require('express')
 const {mongoose}=require("mongoose")
 const app=express()
 require('dotenv').config();
-const port=5410
+const port=process.env.PORT||5410
 
 app.use(express.json())
 const routes = require('./routes/routes');
 app.use('/', routes)
-// const url="mongodb://localhost:27017/person"
-// const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/?retryWrites=true&w=majority"
-const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/testDatabase"
-mongoose.connect(url,{})
+
+mongoose.connect(process.env.URL,{})
     .then(result=>console.log("connected"))
     .catch(err=>console.log(err))
 
