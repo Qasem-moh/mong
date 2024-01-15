02466f77061e94f771eaada41cbd4039dfe98951
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-15 13:33:49 +0300

    mongo

--- server.js
+++ server.js
@@ -10,9 +10,7 @@ app.use('/', routes)
 // const url="mongodb://localhost:27017/person"
 // const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/?retryWrites=true&w=majority"
 const url="mongodb+srv://qasemcoder2028:kGF4HcwbVtiCgf4G@togo20.rzcv24v.mongodb.net/testDatabase"
-mongoose.connect(url,{
-    useNewUrlParser: true,
-})
+mongoose.connect(url,{})
     .then(result=>console.log("connected"))
     .catch(err=>console.log(err))
 
